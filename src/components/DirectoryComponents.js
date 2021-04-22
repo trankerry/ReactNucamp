4c20380c7b919e34ab852fc44a7645776519747d
Kerry <trankerry94@gmail.com>
2021-04-22 00:05:53 -0700

    added breadcrumbs

--- src/components/DirectoryComponents.js
+++ src/components/DirectoryComponents.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
 function RenderDirectoryItem({campsite}) {
@@ -27,6 +27,14 @@ function Directory(props) {
 
     return (
         <div className="container">
+            <div className="row">
+                <div className="col">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Directory</BreadcrumbItem>
+                    </Breadcrumb>
+                </div>
+            </div>
             <div className="row">
                 {directory}
             </div>
